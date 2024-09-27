d301564c7a35816081fdc56e46cfcbe05319b9a5
thyagomac <thyago.cormick@gmail.com>
2024-09-27 19:17:43 -0300

    component PokemonCard.vue created;

---
+++ src/components/cards/PokemonCard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { PokemonType } from '@/types/PokemonType'
+
+const props = defineProps<{
+  pokemon: PokemonType
+}>()
+</script>
+
+<template>
+  <div>--{{ props.pokemon.name }}--</div>
+</template>
--- src/components/cards/PokemonListCards.vue
+++ src/components/cards/PokemonListCards.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { usePokemonStore } from '@/stores/pokemons'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+import PokemonCard from './PokemonCard.vue'
 
 const pokemonStore = usePokemonStore()
+const isLoading = ref(true)
 
 onMounted(async () => {
   const { success, status } = await pokemonStore.dispatchGetPokemons({
@@ -10,14 +12,32 @@ onMounted(async () => {
     offset: 0
   })
 
+  isLoading.value = false
+
   if (!success) {
     alert('Something gone wrong!')
     console.log('Error STATUS: ', status)
   }
 })
+
+const handlePokemonCardClick = () => {
+  console.log('update-pokemon: ')
+}
 </script>
 <template>
   <div class="border border-zinc-200 rounded-md bg-blue-50 p-5">
-    lista: {{ pokemonStore.pokemons }}
+    <div v-show="!isLoading">
+      <h3>Pokemon List</h3>
+      <div class="grid grid-cols-2">
+        <PokemonCard
+          v-for="pokemon in pokemonStore.pokemons"
+          :key="pokemon.name"
+          :pokemon="pokemon"
+          class="cursor-pointer"
+          @click="handlePokemonCardClick"
+        />
+      </div>
+    </div>
+    <div v-show="isLoading">loading</div>
   </div>
 </template>
