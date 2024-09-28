933e783371fa53c3660671c5323f9654f5de8b77
thyagomac <thyago.cormick@gmail.com>
2024-09-28 10:49:20 -0300

    LedBtn created and implemented in HomeView.vue;

---
+++ src/components/buttons/LedBtn.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+type Props = {
+  color?: string
+  size?: string
+  flicker?: boolean
+}
+
+const { size, color, flicker } = withDefaults(defineProps<Props>(), {
+  color: 'cyan',
+  flicker: false,
+  size: '12'
+})
+</script>
+<template>
+  <div
+    :class="`
+      bg-${color}-400
+      rounded-full
+      w-${size}
+      h-${size}
+      border-4
+    border-zinc-200
+      ${flicker ? 'led-flicker' : ''}
+    `"
+  ></div>
+</template>
+
+<style scoped>
+.led-flicker {
+  animation: flicker 1.5s infinite alternate;
+}
+
+/* Flickering animation */
+@keyframes flicker {
+  0%,
+  18%,
+  22%,
+  25%,
+  53%,
+  57%,
+  100% {
+    box-shadow:
+      0 0 4px #fff,
+      0 0 11px #fff,
+      0 0 19px #fff,
+      0 0 20px #22d3ee,
+      0 0 40px #22d3ee,
+      0 0 40px #22d3ee,
+      0 0 45px #22d3ee,
+      0 0 10px #22d3ee;
+  }
+
+  20%,
+  24%,
+  55% {
+    box-shadow: none;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import BaseTemplate from '@/components/layout/BaseTemplate.vue'
 import DescriptionCard from '@/components/cards/DescriptionCard.vue'
 import PokemonListCards from '@/components/cards/PokemonListCards.vue'
 import { onMounted } from 'vue'
+import LedBtn from '@/components/buttons/LedBtn.vue'
 
 onMounted(() => {})
 </script>
@@ -11,9 +12,7 @@ onMounted(() => {})
   <BaseTemplate>
     <div class="flex flex-col gap-5 sm:flex-row bg-red-700 py-5 px-3 rounded-lg">
       <div class="bg-red-600 flex-1">
-        <div
-          class="ml-9 mt-4 bg-cyan-400 rounded-full w-12 h-12 border-4 border-zinc-200 led-blue-flicker"
-        ></div>
+        <LedBtn class="ml-9 mt-4" size="12" color="cyan" :flicker="true" />
         <DescriptionCard />
       </div>
       <div class="bg-green-100">
@@ -22,36 +21,3 @@ onMounted(() => {})
     </div>
   </BaseTemplate>
 </template>
-
-<style>
-.led-blue-flicker {
-  animation: flicker 1.5s infinite alternate;
-}
-
-/* Flickering animation */
-@keyframes flicker {
-  0%,
-  18%,
-  22%,
-  25%,
-  53%,
-  57%,
-  100% {
-    box-shadow:
-      0 0 4px #fff,
-      0 0 11px #fff,
-      0 0 19px #fff,
-      0 0 20px #22d3ee,
-      0 0 40px #22d3ee,
-      0 0 40px #22d3ee,
-      0 0 45px #22d3ee,
-      0 0 10px #22d3ee;
-  }
-
-  20%,
-  24%,
-  55% {
-    box-shadow: none;
-  }
-}
-</style>
