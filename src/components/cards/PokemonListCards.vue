99adf5d26569dc3689758634287016fe3463aa96
thyagomac <thyago.cormick@gmail.com>
2024-09-28 14:54:23 -0300

    search logic in PokemonListCards.vue;

--- src/components/cards/PokemonListCards.vue
+++ src/components/cards/PokemonListCards.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import { usePokemonStore } from '@/stores/pokemons'
 import { computed, onMounted, ref } from 'vue'
-import PokemonCard from './PokemonCard.vue'
+import PokemonCard from '@/components/cards/PokemonCard.vue'
 import type { PokemonType } from '@/types/PokemonType'
-import ListCardsPagination from '../paginations/ListCardsPagination.vue'
+import ListCardsPagination from '@/components/paginations/ListCardsPagination.vue'
+import TextInput from '@/components/inputs/TextInput.vue'
+import Button from '../buttons/Button.vue'
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
 
 const pokemonStore = usePokemonStore()
+const pokemonList = ref<PokemonType[] | null>([])
 const isLoading = ref(true)
 const limit = ref(20)
 const counter = ref(0)
 const disableMoreBtn = ref(false)
+const searchInput = ref('')
 
 const nextPage = computed(() => {
   const nextLimit = limit.value * (counter.value + 1)
@@ -21,6 +26,8 @@ const nextPage = computed(() => {
 
 onMounted(async () => {
   const { success, status } = await pokemonStore.dispatchGetPokemons()
+
+  pokemonList.value = [...pokemonStore.pokemons]
 
   isLoading.value = false
 
@@ -42,6 +49,8 @@ const handleShowMoreTwenty = async () => {
     offset: 0
   })
 
+  pokemonList.value = [...pokemonStore.pokemons]
+
   counter.value++
   isLoading.value = false
 
@@ -60,6 +69,7 @@ const handleShowAll = async () => {
     offset: 0
   })
 
+  pokemonList.value = [...pokemonStore.pokemons]
   isLoading.value = false
 
   if (!success) {
@@ -71,15 +81,32 @@ const handleShowAll = async () => {
 const handlePokemonCardClick = (pokemon: PokemonType) => {
   pokemonStore.updateCurrentPokemon(pokemon)
 }
+
+const handleSearchPokemons = async (e: Event) => {
+  e.preventDefault()
+
+  if (nextPage.value !== 151) {
+    await handleShowAll()
+  }
+
+  let finalResult = pokemonStore.pokemons.filter((pokemon) => {
+    return (
+      pokemon.name.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      pokemon.id.toString().toLowerCase().includes(searchInput.value.toLowerCase().trim())
+    )
+  })
+
+  pokemonList.value = [...finalResult]
+}
 </script>
 <template>
   <div class="bg-red-600 p-5 h-full flex flex-col gap-5">
     <div v-show="!isLoading" class="bg-zinc-300 py-4 px-2 rounded-md">
-      <h2 class="font-bold text-xl mb-3">Pokemon List ({{ pokemonStore.pokemons.length }})</h2>
+      <h2 class="font-bold text-xl mb-3">Pokemon List ({{ pokemonList?.length || 0 }})</h2>
       <div class="pokemon-list-screen">
         <div class="grid grid-cols-2 gap-0 bar">
           <PokemonCard
-            v-for="pokemon in pokemonStore.pokemons"
+            v-for="pokemon in pokemonList"
             :key="pokemon.name"
             :pokemon="pokemon"
             @click="() => handlePokemonCardClick(pokemon)"
@@ -87,12 +114,25 @@ const handlePokemonCardClick = (pokemon: PokemonType) => {
         </div>
       </div>
     </div>
-    <div v-show="isLoading">loading</div>
+    <div v-show="isLoading">loading...</div>
 
+    <form @submit="handleSearchPokemons">
+      <div class="flex">
+        <TextInput
+          id="search-pokemon-input"
+          class="flex-1"
+          placeholder="Search a pokemon"
+          v-model="searchInput"
+        />
+        <Button type="submit">
+          <MagnifyingGlassIcon class="block h-6 w-6" />
+        </Button>
+      </div>
+    </form>
     <ListCardsPagination
       :nextPage="nextPage"
-      @handleShowAll="handleShowAll"
       @handleShowMoreTwenty="handleShowMoreTwenty"
+      @handleShowAll="handleShowAll"
     />
   </div>
 </template>
--- src/components/inputs/TextInput.vue
+++ src/components/inputs/TextInput.vue
@@ -1,15 +1,50 @@
 <script setup lang="ts">
-const props = defineProps<{
+const { variant, size, type } = defineProps<{
+  id: string
   variant?: string
   size?: string
   type?: string
-  value: string
+  placeholder?: string
+  labelText?: string
 }>()
 
-const variantClass =
-  props.variant === 'secondary' ? 'bg-green-200 hover:bg-green-400' : 'bg-red-200 hover:bg-red-400'
+const model = defineModel()
+
+const loadVariantClass = () => {
+  if (variant && variant === 'secondary') {
+    return 'rounded-md bg-green-200 hover:bg-green-400 disabled:hover:bg-green-200'
+  }
+  if (variant && variant === 'dark-btn') {
+    return 'rounded-md bg-zinc-700 text-zinc-400 border-4 border-zinc-900 hover:bg-zinc-900 disabled:hover:bg-zinc-700 hover:text-zinc-100 disabled:hover:text-zinc-400'
+  }
+  return 'bg-blue-100 hover:bg-blue-200'
+}
+
+const loadSizeClass = () => {
+  if (size && size === 'sm') {
+    return 'py-1 px-2 text-sm'
+  }
+
+  if (size && size === 'lg') {
+    return 'py-3 px-4 text-lg'
+  }
+
+  return 'py-2 px-3 '
+}
+
+const variantClass = loadVariantClass()
+const sizeClass = loadSizeClass()
 </script>
 
 <template>
-  <input :type="type ? type : 'text'" :class="['py-2 px-3 w-min rounded-md', variantClass]" />
+  <div class="flex flex-col gap-2">
+    <label :hidden="labelText ? false : true" :for="id">{{ labelText || id }}</label>
+    <input
+      :id="id"
+      :type="type ? type : 'text'"
+      :class="['py-2 px-3 w-min outline-none', variantClass, sizeClass]"
+      :placeholder="placeholder"
+      v-model="model"
+    />
+  </div>
 </template>
